91220d58dd97d556993fb6c0f95dc95868740f5a
jonnty <jonnty@gmail.com>
2009-08-12 13:55:45 +0100

    Added ftp logging facillities.

--- playlist/ftp.py
+++ playlist/ftp.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import threading
+import time
 
 sys.path.append('/home/jonnty/Python/pydj/pydj/playlist')
 sys.path.append('/home/jonnty/Python/pydj/')
@@ -45,10 +46,24 @@ class G2Authorizer(ftpserver.DummyAuthorizer):
       
     return bool(authenticate(username=username, password=password))
   
+now = lambda: time.strftime("[%Y-%b-%d %H:%M:%S]")
+   
+def standard_logger(msg):
+    f1.write("%s %s\n" %(now(), msg))
+    f1.flush()
+   
+def line_logger(msg):
+    f2.write("%s %s\n" %(now(), msg))
+    f2.flush()
+  
 if __name__ == "__main__":
-    authorizer = G2Authorizer()
-    ftp_handler = G2FTPHandler
-    ftp_handler.authorizer = authorizer
-    address = ('', 2100)
-    ftpd = ftpserver.FTPServer(address, ftp_handler)
-    ftpd.serve_forever()
+  f1 = open('ftpd.log', 'a')
+  f2 = open('ftpd.lines.log', 'a')
+  ftpserver.log = standard_logger
+  ftpserver.logline = line_logger
+  authorizer = G2Authorizer()
+  ftp_handler = G2FTPHandler
+  ftp_handler.authorizer = authorizer
+  address = ('', 2100)
+  ftpd = ftpserver.FTPServer(address, ftp_handler)
+  ftpd.serve_forever()
