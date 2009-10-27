38ff3cf175b147a5c8693c8c5c9fa95d247ba57e
jonnty <jonnty@gmail.com>
2009-10-27 23:10:41 +0000

    Extended search system to allow more freeform modifier usage and addition.

--- playlist/search.py
+++ playlist/search.py
@@ -8,10 +8,13 @@ import StringIO
 class Search:
   
   def __init__(self, query):
-    self.actions = {"artist:": lambda s: Q(artist__name__icontains=s),
-                    "album:": lambda s: Q(album__name__icontains=s),
-                    "title:": lambda s: Q(title__icontains=s),
+    self.actions = {"artist": "artist__name",
+                    "album": "album__name",
+                    "title": "title",
                     }
+    self.modifiers = {"=": "__iexact",
+                      ":": "__icontains"}
+    
     tokens = [Str(token) for token in self.actions.keys()] 
     self.lexicon = Lexicon([(Alt(*tokens), "token"),
                             (Rep1(AnyBut(" ")), "term"),
@@ -20,7 +23,7 @@ class Search:
     self.query = query
     
   def _parseString(self, query=None):
-    """Returns a parsed tuple list representing query"""
+    """Return a parsed tuple list representing query"""
     if query is None:
       query = self.query
     input = StringIO.StringIO()
@@ -30,34 +33,91 @@ class Search:
     last = s.read()
     tuples = []
     while last[0]: #read up till final None
-      tuples.append(last)
+      print last
+      if last[0] == "term":
+        last = self._parseTerm(last)
+      else:
+        last = [last] #to make extend()) call work
+      tuples.extend(last)
       last = s.read()
     return tuples
   
+  def _parseTerm(self, (type, arg)):
+    """Find modifiers (and actions) in a term tuple, and appropriately parses them
+    
+    Return tuple list suitable for extension onto parsing results"""
+    
+    for modifier in self.modifiers.keys():
+      if arg.find(modifier) > -1:
+        term = list(arg.partition(modifier))
+        return  self._parsePartitionedTerm(term)
+      
+    return [(type, arg)]
+        
+  def _parsePartitionedTerm(self, term):
+    """Convert a term that has been partitioned by parseTerm into a nice list"""
+    #work backwards as we may be deleting elements which would muck things up otehrwise
+    if len(term[2]) == 0:
+      del term[2]
+    else:
+      term[2] = ('term', term[2])
+    term[1] = ('modifier', term[1]) #assume we've been given valid input
+    if len(term[0]) == 0:
+      del term[0]
+    elif term[0] in self.actions.keys():
+      term[0] = ("token", term[0])
+    else:
+      term[0] = ("term", term[0]) 
+    return term
+        
+  
   def _makeQuery(self, tuples):
-    """Returns a list of queries ready to be used like so: Song.objects.filter(*query)"""
-    queries = []
-    start = 0
-    action = lambda s: Q(title__icontains=s)
+    """Returns a list of queries ready to be used like so: Song.objects.filter(**query)"""
+    queries = [] #list of argument name, argument tuples for filter
+    action = "title__icontains" #default action
     term = []
-    for type, arg in tuples:
+    print tuples
+    for i, (type, arg) in enumerate(tuples):
       if type == "term":
         term.append(arg) #part of a search string
       elif type == "token":
-        queries.append(action(" ".join(term))) #sort out last search term
-        action = self.actions[arg] #set up stuff to be done to next lot of terms
-        term = [] #empty search terms list
+        try:
+          next = tuples[i+1]
+        except IndexError:
+          term.append(arg) #action is meant as a term
+          continue
+        
+        if next[0] == 'modifier': #check whether next tuple is modifier
+          if len(term):
+            queries.append((action, " ".join(term))) #sort out last search term
+          action = self.actions[arg] + self.modifiers[next[1]]
+          modifier_handled = True #tell next iteration to ignore modifier
+          term = [] #empty search terms list
+        else: #not to be treated as token
+          term.append(arg)
+           
+        
+      elif type == "modifier":
+        if modifier_handled: #handled already: ignore (but reset flag)
+          modifier_handled = False
+        else: #just normal term
+          term.append(arg) 
     if term: #still got some clearing up to do
-      queries.append(action(" ".join(term)))
-      
-    return queries
+      queries.append((action, " ".join(term)))
+    
+    #convert to dict for passing to filter()
+    dict = {}
+    dict.update(queries)
+    return dict 
   
   def getResults(self):
     query = self._makeQuery(self._parseString())
-    return Song.objects.select_related().filter(*query)
+    return Song.objects.select_related().filter(**query)
       
     
 if __name__ == "__main__":
-  s = Search("electioneering artist: radiohead")
+  s = Search("title = electioneering")
+  print s._makeQuery(s._parseString())
   print s.getResults()
+
   
