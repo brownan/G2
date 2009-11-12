42cfac2464117e5449054910dff522bf85d1722a
jonnty <jonnty@gmail.com>
2009-11-12 02:13:39 +0000

    Extracted forms from views.py and tried to make playlist less project.dependant

---
+++ playlist/forms.py
@@ -0,0 +1,77 @@
+from django import forms
+from django.contrib.auth.models import User,  UserManager, Group, Permission
+from django.contrib.auth.forms import UserCreationForm
+
+from playlist.utils import getObj
+from playlist.models import *
+
+
+class UploadFileForm(forms.Form):
+  file  = forms.FileField()
+
+class SearchForm(forms.Form):
+  query = forms.CharField(max_length=100)
+  orphan = forms.BooleanField(required=False, initial=False)
+  
+  def clean_query(self):
+    if len(self.cleaned_data['query']) < 3:
+      raise forms.ValidationError, "Query should be 3 characters long or more."
+    return self.cleaned_data['query']
+  
+class SongForm(forms.ModelForm):
+
+  artist = forms.CharField(max_length=300) #replace dropdowns with charfields
+  album = forms.CharField(max_length=300) #they should be filled in the template 
+  class Meta:
+    model = Song
+    
+  
+  def save(self):
+    self.cleaned_data['artist'] = getObj(Artist, self.cleaned_data['artist'], self.initial['artist']) #convert strings to objects
+    self.cleaned_data['album'] = getObj(Album, self.cleaned_data['album'])
+    forms.ModelForm.save(self)
+  
+
+class SettingsForm(forms.ModelForm):
+  class Meta:
+    #model = UserProfile
+    fields = ()
+    
+#class SearchForm(forms.Form):
+ # pass
+    
+class CommentForm(forms.Form):
+  comment = forms.CharField(max_length=400)
+  
+class BanForm(forms.Form):
+  reason = forms.CharField(max_length=100)
+  
+class RegisterForm(UserCreationForm):
+  passcode = forms.CharField(max_length=50)
+
+  
+class NewRegisterForm(forms.Form):
+  saname = forms.CharField(max_length=30, label="SA Username:")
+  password1 = forms.CharField(max_length=30, label="Desired Password:", widget=forms.PasswordInput)
+  password2 = forms.CharField(max_length=30, label="Confirm Password:", widget=forms.PasswordInput)
+  email = forms.EmailField(label="E-mail Address:")
+  randcode = forms.CharField(max_length=32, widget=forms.HiddenInput)
+      
+  def clean(self):
+    if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+      raise forms.ValidationError, "Passwords must match"
+    if len(self.cleaned_data['password1']) < 4:
+      raise forms.ValidationError, "Password too short: must be at least 4 characters"
+    
+    return self.cleaned_data
+  
+  def clean_saname(self):
+    try:
+      User.objects.get(username=self.cleaned_data['saname'])
+      raise forms.ValidationError, "Username already registered. If you're not happy about this, PM Jonnty."
+    except User.DoesNotExist:
+      return self.cleaned_data['saname'] 
+      
+  
+class WelcomeMsgForm(forms.Form):
+  message = forms.CharField(max_length=2000, widget=forms.Textarea)
--- playlist/views.py
+++ playlist/views.py
@@ -23,10 +23,7 @@ import itertools
 import datetime
 from random import getrandbits
 from hashlib import md5
-from urllib2 import urlopen, URLError
-import urllib2
-import cookielib
-from urllib import quote, urlencode
+from urllib2 import URLError
 from subprocess import Popen
 import logging
 
@@ -35,12 +32,9 @@ from django.template import Context, loader
 from django.core.urlresolvers import reverse
 from django.core.serializers import serialize
 from django.contrib.auth.models import User,  UserManager, Group, Permission
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
 from django.shortcuts import render_to_response
 import django.contrib.auth.views
 import django.contrib.auth as auth
-from django.forms.models import modelformset_factory
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.template import RequestContext
 from django.conf import settings
@@ -48,12 +42,17 @@ from django.db import connection, transaction
 from django.db.models import Avg, Max, Min, Count, Q
 from django.contrib.auth import authenticate
 from django.db import connection
+from django.contrib.auth.decorators import permission_required, login_required
 
-from pydj.playlist.models import *
-from pydj.playlist.utils import getSong, getObj, listenerCount
-from pydj.playlist.upload import UploadedFile
-from pydj.playlist.search import Search
+
+from playlist.forms import *
+from playlist.models import *
+from playlist.utils import getSong, getObj, listenerCount
+from playlist.upload import UploadedFile
+from playlist.search import Search
 from sa import SAProfile
+
+
 
 
 permissions = ["upload_song", "view_artist", "view_playlist", "view_song", "view_user", "queue_song"]
@@ -62,84 +61,9 @@ PIDFILE = settings.LOGIC_DIR+"/pid"
 SA_PREFIX = "http://forums.somethingawful.com/member.php?action=getinfo&username="
 LIVE = settings.IS_LIVE
 
-from django.contrib.auth.decorators import permission_required, login_required
 
 def now():
   return " ".join(datetime.datetime.now().isoformat().split("T"))
-
-class UploadFileForm(forms.Form):
-  file  = forms.FileField()
-
-class SearchForm(forms.Form):
-  query = forms.CharField(max_length=100)
-  orphan = forms.BooleanField(required=False, initial=False)
-  
-  def clean_query(self):
-    if len(self.cleaned_data['query']) < 3:
-      raise forms.ValidationError, "Query should be 3 characters long or more."
-    return self.cleaned_data['query']
-  
-class SongForm(forms.ModelForm):
-
-  artist = forms.CharField(max_length=300) #replace dropdowns with charfields
-  album = forms.CharField(max_length=300) #they should be filled in the template 
-  class Meta:
-    model = Song
-    #exclude = ("album", "artist")
-    
-  def populate(self, song):pass
-    #self.artist = forms.CharField(max_length=300, initial=song.artist.name)
-    #self.album = forms.CharField(max_length=300, initial=song.album.name)
-  
-  def save(self):
-    self.cleaned_data['artist'] = getObj(Artist, self.cleaned_data['artist'], self.initial['artist']) #convert strings to objects
-    self.cleaned_data['album'] = getObj(Album, self.cleaned_data['album'])
-    forms.ModelForm.save(self)
-  
-
-class SettingsForm(forms.ModelForm):
-  class Meta:
-    #model = UserProfile
-    fields = ()
-    
-#class SearchForm(forms.Form):
- # pass
-    
-class CommentForm(forms.Form):
-  comment = forms.CharField(max_length=400)
-  
-class BanForm(forms.Form):
-  reason = forms.CharField(max_length=100)
-  
-class RegisterForm(UserCreationForm):
-  passcode = forms.CharField(max_length=50)
-
-  
-class NewRegisterForm(forms.Form):
-  saname = forms.CharField(max_length=30, label="SA Username:")
-  password1 = forms.CharField(max_length=30, label="Desired Password:", widget=forms.PasswordInput)
-  password2 = forms.CharField(max_length=30, label="Confirm Password:", widget=forms.PasswordInput)
-  email = forms.EmailField(label="E-mail Address:")
-  randcode = forms.CharField(max_length=32, widget=forms.HiddenInput)
-      
-  def clean(self):
-    if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-      raise forms.ValidationError, "Passwords must match"
-    if len(self.cleaned_data['password1']) < 4:
-      raise forms.ValidationError, "Password too short: must be at least 4 characters"
-    
-    return self.cleaned_data
-  
-  def clean_saname(self):
-    try:
-      User.objects.get(username=self.cleaned_data['saname'])
-      raise forms.ValidationError, "Username already registered. If you're not happy about this, PM Jonnty."
-    except User.DoesNotExist:
-      return self.cleaned_data['saname'] 
-      
-  
-class WelcomeMsgForm(forms.Form):
-  message = forms.CharField(max_length=2000, widget=forms.Textarea)
 
   
 @permission_required('playlist.start_stream')
